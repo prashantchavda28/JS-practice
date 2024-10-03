9b189605aaf511ccb0abb04c730230f1bc9b85a4
Prashant Chavda <139984452+prashantchavda28@users.noreply.github.com>
2024-10-03 05:38:51 +0000

    scope

--- 03_basics/01_fuction.js
+++ 03_basics/01_fuction.js
@@ -22,7 +22,7 @@ function addTwoNumbers(number1, number2){
     return number1 + number2
 }
 
-console.log(addTwoNumbers(3, 5));
+// console.log(addTwoNumbers(3, 5));
  
 
 // // console.log("Result: ", result);
@@ -61,11 +61,11 @@ console.log(addTwoNumbers(3, 5));
 //     price: 399
 // })
 
-// const myNewArray = [200, 400, 100, 600]
+const myNewArray = [200, 400, 100, 600]
 
-// function returnSecondValue(getArray){
-//     return getArray[1]
-// }
+function returnSecondValue(getArray){
+    return getArray[1]
+}
 
-// // console.log(returnSecondValue(myNewArray));
-// // console.log(returnSecondValue([200, 400, 500, 1000]));
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
